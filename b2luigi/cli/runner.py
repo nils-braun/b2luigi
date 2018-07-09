80a57917334de489caaf05dcdeca4c773cec3fa1
Nils Braun <nils.braun@kit.edu>
2018-07-09 10:29:36 +0200

    Added the show_outputs function

--- b2luigi/cli/runner.py
+++ b2luigi/cli/runner.py
@@ -1,12 +1,14 @@
+import collections
 import json
 
 import luigi
 import luigi.server
 import luigi.configuration
+import os
 
 from b2luigi.batch.workers import SendJobWorkerSchedulerFactory
 from b2luigi.core.settings import set_setting
-from b2luigi.core.utils import task_iterator
+from b2luigi.core.utils import task_iterator, get_all_output_files_in_tree
 
 
 def run_as_batch_worker(task_list, cli_args, kwargs):
@@ -43,5 +45,25 @@ def run_test_mode(task_list, cli_args, kwargs):
     set_setting("dispatch", False)
     luigi.build(task_list, log_level="DEBUG", local_scheduler=True, **kwargs)
 
-def show_all_outputs(*args, **kwargs):
-    raise NotImplementedError
+def show_all_outputs(task_list, *args, **kwargs):
+    from colorama import Fore, Style
+
+    all_output_files = collections.defaultdict(list)
+
+    for task in task_list:
+        output_files = get_all_output_files_in_tree(task)
+        for key, file_names in output_files.items():
+            all_output_files[key] += file_names
+
+    for key, file_names in all_output_files.items():
+        print(key)
+
+        file_names = sorted(set(d["file_name"] for d in file_names))
+        for file_name in file_names:
+            # TODO: this is not correct as it does not check the task status!
+            if os.path.exists(file_name):
+                print("\t", Fore.GREEN, file_name, Style.RESET_ALL)
+            else:
+                print("\t", Fore.RED, file_name, Style.RESET_ALL)
+        print()
+
