27b19885d5033a90de2d317011e2f73612eedd2c
Nils Braun <nils.braun@kit.edu>
2018-08-02 23:09:43 +0200

    Make the dispatching easier by givin a wrapper

--- b2luigi/core/dispatchable_task.py
+++ b2luigi/core/dispatchable_task.py
@@ -9,6 +9,28 @@ from b2luigi.core.settings import get_setting
 from b2luigi.core.task import Task
 from b2luigi.core.utils import get_log_files
 
+def dispatch(run_function):
+    def wrapped_run_function(self):
+        if get_setting("local_execution", False):
+            self.create_output_dirs()
+            run_function(self)
+        else:
+            filename = os.path.realpath(sys.argv[0])
+            stdout_file_name, stderr_file_name = get_log_files(self)
+
+            executable = get_setting("executable", [sys.executable])
+
+            with open(stdout_file_name, "w") as stdout_file:
+                with open(stderr_file_name, "w") as stderr_file:
+                    return_code = subprocess.call(self.cmd_prefix + executable + [os.path.basename(filename), "--batch-runner", "--task-id", self.task_id],
+                                                stdout=stdout_file, stderr=stderr_file,
+                                                cwd=os.path.dirname(filename))
+
+            if return_code:
+                raise RuntimeError(f"Execution failed with return code {return_code}")
+
+    return wrapped_run_function
+
 
 class DispatchableTask(Task):
     def __init__(self, *args, **kwargs):
@@ -19,27 +41,7 @@ class DispatchableTask(Task):
         raise NotImplementedError
 
     def run(self):
-        if get_setting("local_execution", False):
-            self._run_local()
-        else:
-            self._dispatch()
-
-    def _run_local(self):
-        self.create_output_dirs()
-        self.process()
-
-    def _dispatch(self):
-        filename = os.path.realpath(sys.argv[0])
-        stdout_file_name, stderr_file_name = get_log_files(self)
-
-        with open(stdout_file_name, "w") as stdout_file:
-            with open(stderr_file_name, "w") as stderr_file:
-                return_code = subprocess.call(self.cmd_prefix + [sys.executable, os.path.basename(filename), "--batch-runner", "--task-id", self.task_id],
-                                              stdout=stdout_file, stderr=stderr_file,
-                                              cwd=os.path.dirname(filename))
-
-        if return_code:
-            raise RuntimeError(f"Execution failed with return code {return_code}")
+        dispatch(self.__class__.process)(self)
 
     def on_failure(self, exception):
         stdout_file_name, stderr_file_name = get_log_files(self)
