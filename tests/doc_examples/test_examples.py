5f8b876ca72bb2c410934a32b9d6712db39e07e2
Nils Braun <nils.braun@kit.edu>
2018-08-04 19:19:59 +0200

    Fixed a test which was running locally but not on travis.ci

--- tests/doc_examples/test_examples.py
+++ tests/doc_examples/test_examples.py
@@ -5,17 +5,11 @@ from ..helpers import B2LuigiTestCase
 
 
 class ExampleTestCase(B2LuigiTestCase):
-    pass
+    def test_examples(self):
+        for file_name in glob(os.path.join(os.path.dirname(__file__), "*.py")):
+            short_file_name = os.path.basename(file_name)
 
+            if short_file_name.startswith("test_") or short_file_name.startswith("__"):
+                continue
 
-for file_name in glob(os.path.join(os.path.dirname(__file__), "*.py")):
-    def lambda_function(self):
-        self.call_file(os.path.join("doc_examples/", os.path.basename(file_name)))
-
-    short_file_name = os.path.basename(file_name)
-    short_file_name = os.path.splitext(short_file_name)[0]
-
-    if short_file_name.startswith("test_") or short_file_name.startswith("__"):
-        continue
-
-    setattr(ExampleTestCase, "test_" + short_file_name, lambda_function)
+            self.call_file(os.path.join("doc_examples/", os.path.basename(file_name)))
