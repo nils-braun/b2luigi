2dd45f1c0427ad1340247068864bc30e77e9e972
Nils Braun <nils.braun@kit.edu>
2018-08-08 20:02:01 +0200

    Remove the assert for the basf2_release to make it also usable for non-basf2 usages.

--- b2luigi/basf2_helper/utils.py
+++ b2luigi/basf2_helper/utils.py
@@ -6,8 +6,6 @@ import git
 def get_basf2_git_hash():
     basf2_release = os.getenv("BELLE2_RELEASE")
 
-    assert basf2_release
-
     if basf2_release == "head":
         basf2_release_location = os.getenv("BELLE2_LOCAL_DIR")
 
