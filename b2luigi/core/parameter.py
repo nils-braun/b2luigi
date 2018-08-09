b2a0ab2f5c0556cbcae0c5462e668e0fc5832ee7
Nils Braun <nils.braun@kit.edu>
2018-08-09 18:00:02 +0200

    Only serialize when generating a file path

--- b2luigi/core/parameter.py
+++ b2luigi/core/parameter.py
@@ -4,8 +4,5 @@ import b2luigi
 
 
 class HashedParameter(b2luigi.Parameter):
-    def serialize(self, x):
+    def serialize_hashed(self, x):
         return "hashed_" + hashlib.md5(str(x).encode()).hexdigest()
-
-    def parse(self, x):
-        raise AttributeError("A hashed parameter can not be parsed")
