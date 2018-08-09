09988358b10bd8dbe4a21611665c07c2fb9fadc8
Nils Braun <nils.braun@kit.edu>
2018-08-09 10:34:14 +0200

    Better add a hashed prefix for differentiating the parameters

--- b2luigi/core/parameter.py
+++ b2luigi/core/parameter.py
@@ -5,7 +5,7 @@ import b2luigi
 
 class HashedParameter(b2luigi.Parameter):
     def serialize(self, x):
-        return hashlib.md5(str(x).encode()).hexdigest()
+        return "hashed_" + hashlib.md5(str(x).encode()).hexdigest()
 
     def parse(self, x):
         raise AttributeError("A hashed parameter can not be parsed")
--- tests/core/test_parameter.py
+++ tests/core/test_parameter.py
@@ -11,10 +11,10 @@ class HashedParameterTestCase(TestCase):
         serialized_second = first_parameter.serialize({"key1": "value1", "key2": 12345})
 
         self.assertEqual(serialized_first, serialized_second)
-        self.assertEqual(serialized_first, "df6221c515cbb93735f9478cb05a00e4")
+        self.assertEqual(serialized_first, "hashed_df6221c515cbb93735f9478cb05a00e4")
 
         serialized_first = first_parameter.serialize([1, "test", 456, {"hello": "bye"}])
         serialized_second = first_parameter.serialize([1, "test", 457, {"hello": "bye"}])
 
         self.assertNotEqual(serialized_first, serialized_second)
-        self.assertEqual(serialized_first, "7816c14282fd03e3dc4e398f28aa5a30")
+        self.assertEqual(serialized_first, "hashed_7816c14282fd03e3dc4e398f28aa5a30")
