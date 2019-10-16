86c7533754e6d93c4cbad4a2c85eda50269d75ac
Maximilian Welsch <mwelsch@uni-bonn.de>
2019-10-16 17:51:00 +0200

    add htcondorjobstatus cache

--- b2luigi/batch/processes/htcondor.py
+++ b2luigi/batch/processes/htcondor.py
@@ -1,13 +1,15 @@
 import json
+import logging
+import os
 import re
+import stat
 import subprocess
 import enum
+import sys
 
-
+from b2luigi.core.utils import get_setting, get_output_dirs
 from b2luigi.batch.processes import BatchProcess, JobStatus, BatchJobStatusCache
 from b2luigi.core.utils import get_log_file_dir
-
-from cachetools import TTLCache
 
 
 class HTCondorJobStatusCache(BatchJobStatusCache):
@@ -15,21 +17,35 @@ class HTCondorJobStatusCache(BatchJobStatusCache):
     def __init__(self):
         super(HTCondorJobStatusCache, self).__init__()
 
-    def _ask_for_job_status(self, job_id=None):
-        cmd = ["condor_q", "-json", "-attributes", "ClusterId,ProcId,JobStatus"]
+    def _ask_for_job_status(self, job_id: int = None):
+        q_cmd = ["condor_q", "-json", "-attributes", "ClusterId,ProcId,JobStatus"]
+        history_cmd = ["condor_history", "-json", "-attributes", "ClusterId,ProcId,JobStatus"]
 
         if job_id:
-            cmd.append(str(job_id))
-            output = subprocess.check_output(cmd)
+            q_cmd.append(str(job_id))
+            output = subprocess.check_output(q_cmd)
         else:
-            output = subprocess.check_output(cmd)
-
+            output = subprocess.check_output(q_cmd)
         output = output.decode()
-        dict_list = [json.loads(status_dict) for status_dict
-                     in output.lstrip("[\n").rstrip("\n]\n").split("\n,\n")]
+
+        dict_list = []
+        for status_dict in output.lstrip("[\n").rstrip("\n]\n").split("\n,\n"):
+            if status_dict:
+                dict_list.append(json.loads(status_dict))
+            else:
+                continue
+
+        if job_id:
+            if job_id not in [status_dict["ClusterId"] for status_dict in dict_list]:
+                history_cmd.append(str(job_id))
+                output = subprocess.check_output(history_cmd)
+                output = output.decode()
+                output = output.lstrip("[\n").rstrip("\n]\n").split("\n,\n")
+                if output:
+                    dict_list.append(json.loads(output))
 
         for status_dict in dict_list:
-            self[status_dict["ClusterId"]] = status_dict["JobStatus"]
+            self[status_dict["ClusterId"]] = status_dict["JobStatus"]  # int -> int
 
 
 class HTCondorJobStatus(enum.Enum):
