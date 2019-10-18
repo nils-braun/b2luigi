e7501522dfb6224b26dee886e33e99bec2678ccd
Maximilian Welsch <mwelsch@uni-bonn.de>
2019-10-18 09:06:40 +0200

    add a htcondor example folder for convenience

---
+++ tests/htcondor/simple_example_b2luigi.py
@@ -0,0 +1,19 @@
+import b2luigi
+import random
+
+
+class MyNumberTask(b2luigi.Task):
+    some_parameter = b2luigi.IntParameter()
+
+    def output(self):
+        yield self.add_to_output("output_file.txt")
+
+    def run(self):
+        random_number = random.random()
+
+        with open(self.get_output_file_name("output_file.txt"), "w") as f:
+            f.write(f"{random_number}\n")
+
+
+if __name__ == "__main__":
+    b2luigi.process([MyNumberTask(some_parameter=i) for i in range(10)], batch=True, workers=10)
