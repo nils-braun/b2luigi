19a864c4b0e118b6c51c3005682f73788adf2be9
Nils Braun <nils.braun@kit.edu>
2019-10-25 14:23:43 +0200

    Do not raise an exception to make the module still importable

--- b2luigi/basf2_helper/utils.py
+++ b2luigi/basf2_helper/utils.py
@@ -9,7 +9,9 @@ def get_basf2_git_hash():
     if basf2_release == "head" or basf2_release is None:
         basf2_release_location = os.getenv("BELLE2_LOCAL_DIR")
 
-        assert basf2_release_location
-        return git.Repo(basf2_release_location).head.object.hexsha
+        if basf2_release_location:
+            return git.Repo(basf2_release_location).head.object.hexsha
+        else:
+            return "not_set"
 
     return basf2_release
