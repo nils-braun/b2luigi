de28bf7b8badae2bd6947f8c9491e19e93ff6fde
Nils Braun <nils.braun@kit.edu>
2019-10-25 14:55:20 +0200

    Added two tests for the folder structure assumptions

---
+++ tests/core/test_folder_structures.py
@@ -0,0 +1,27 @@
+import os
+
+from ..helpers import B2LuigiTestCase
+
+import b2luigi
+
+
+class TemporaryWrapperTestCase(B2LuigiTestCase):
+    def test_result_path(self):
+        self.call_file("core/folder_structures.py")
+
+        self.assertTrue(os.path.exists("results/test.txt"))
+
+        with open("results/test.txt", "r") as f:
+            self.assertEqual(f.read(), "Test")
+
+
+    def test_result_path_from_other_location(self):
+        os.makedirs("some_other_folder", exist_ok=True)
+        self.call_file("core/folder_structures.py", cwd="some_other_folder")
+
+        self.assertTrue(os.path.exists("results/test.txt"))
+        self.assertTrue(os.path.exists("some_other_folder"))
+        self.assertFalse(os.path.exists("some_other_folder/results/test.txt"))
+
+        with open("results/test.txt", "r") as f:
+            self.assertEqual(f.read(), "Test")
