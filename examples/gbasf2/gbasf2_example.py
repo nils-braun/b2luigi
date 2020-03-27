5df87d20175d69e7b4cef3a1dcc488f68a7c2e1d
Michael Eliachevitch <meliache@uni-bonn.de>
2020-03-27 16:15:18 +0100

    Add settings file, to use it in combination with task settings

--- examples/gbasf2/gbasf2_example.py
+++ examples/gbasf2/gbasf2_example.py
@@ -8,11 +8,10 @@ import example_mdst_analysis
 
 class MyAnalysisTask(Basf2PathTask):
     batch_system = "gbasf2"
-    gbasf2_release = "light-2002-janus"
-    gbasf2_project_name = "b2luigi_gbasf2_example"
-    gbasf2_cputime = 30  # expected time in minutes
+    gbasf2_project_name = "b2luigi_gbasf2_example_5"
+    gbasf2_cputime = 10  # expected time in minutes
     gbasf2_input_dataset = join("/belle/MC/release-04-00-03/DB00000757/MC13a/prod00009434/s00/e1003/4S/",
-                                "r00000/mixed/mdst/sub00/mdst_000255_prod00009434_task10020000255.root")
+                                "r00000/mixed/mdst/sub00")
     max_event = 200  # limit number of events for testing
 
     def create_path(self):
