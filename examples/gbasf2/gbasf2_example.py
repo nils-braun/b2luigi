e3af9d15392398d98988017e22c5ca7d279a1c78
Michael Eliachevitch <meliache@uni-bonn.de>
2020-03-27 16:50:21 +0100

    More comments in example script plus small changes

--- examples/gbasf2/gbasf2_example.py
+++ examples/gbasf2/gbasf2_example.py
@@ -7,12 +7,15 @@ import example_mdst_analysis
 
 
 class MyAnalysisTask(Basf2PathTask):
+    # We define some settings for the task. These can also alternatively be set for all tasks in the settings.json
     batch_system = "gbasf2"
-    gbasf2_project_name = "b2luigi_gbasf2_example_5"
-    gbasf2_cputime = 10  # expected time in minutes
+    # note: make sure to use a unique project name not used before
+    gbasf2_project_name = "b2luigi_gbasf2_example"
+    gbasf2_cputime = 5  # expected time per job in minutes
+    gbasf2_priority = 5
     gbasf2_input_dataset = join("/belle/MC/release-04-00-03/DB00000757/MC13a/prod00009434/s00/e1003/4S/",
-                                "r00000/mixed/mdst/sub00")
-    max_event = 200  # limit number of events for testing
+                                "r00000/mixed/mdst/sub00/mdst_000255_prod00009434_task10020000255.root")
+    max_event = 100  # limit number of events for testing
 
     def create_path(self):
         return example_mdst_analysis.create_analysis_path()
