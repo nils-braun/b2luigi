f7acf9a48bb34dbc446fa0995f1466308a063c9b
Michael Eliachevitch <meliache@uni-bonn.de>
2020-04-20 21:24:11 +0200

    Update example: New documentation and iterate over input datasets

--- examples/gbasf2/gbasf2_example.py
+++ examples/gbasf2/gbasf2_example.py
@@ -7,21 +7,21 @@ import example_mdst_analysis
 
 
 class MyAnalysisTask(Basf2PathTask):
+    # set the batch_system property to use the gbasf2 wrapper batch process for this task
     batch_system = "gbasf2"
-
-    mbc_range = b2luigi.ListParameter(hashed=True)
-
-    # Common gbasf2 project name prefix instances of this task b2luigi will
-    # create a gbasf2 project name by appending a unique hash derived from the
-    # luigi paramater to make sure that each task has a unique project name as
-    # long as you don't restart a task without changing parameters
+    # Must define a prefix for the gbasf2 project name to submit to the grid.
+    # b2luigi will then add a hash derived from the luigi parameters to create a unique project name.
     gbasf2_project_name_prefix = b2luigi.Parameter(significant=False)
-
     gbasf2_input_dataset = b2luigi.Parameter(hashed=True)
     # Define some more settings as class properties. Alternatively, they could
     # also be defined in the settings.json
     gbasf2_download_dir = "."
-    gbasf2_cputime = 5  # expected time per job in minutes
+    gbasf2_cputime = 5  # expected time per job in minutes, we use a low one b/c we set max_event to a low value
+
+    # As an example we define a cut range as a b2luigi ListParameter, so that we
+    # can automatically create multiple tasks and thus multiple independent
+    # gbasf2 projects for differents sets of cuts
+    mbc_range = b2luigi.ListParameter(hashed=True)
 
     def create_path(self):
         return example_mdst_analysis.create_analysis_path(mbc_range=self.mbc_range)
@@ -32,12 +32,6 @@ class MyAnalysisTask(Basf2PathTask):
         command, which is wrapped by the gbasf2 batch system, downloads the
         dataset.  It is defined by the contatenation of the
         ``gbasf2_download_dir`` and ``gbasf2_project_name`` settings.
-
-        Warning: That means that the job is also done, if the project directory
-        had been created, but is not containing any or all files.  The files are
-        downloaded only if the project is fully successful, so I am relying on
-        the download not to fail.  If the target exists but you want to re-run
-        the task, just delete it by hand.
         """
         gbasf2_project_name = get_unique_gbasf2_project_name(self)
         gbasf2_dataset_dir = os.path.join(self.gbasf2_download_dir, gbasf2_project_name)
@@ -45,18 +39,26 @@ class MyAnalysisTask(Basf2PathTask):
 
 
 class MasterTask(b2luigi.WrapperTask):
+    """
+    We use the MasterTask to require multiple analyse tasks with different input
+    datasets and cut values.  For each parameter combination, a different gbasf2
+    project will be submitted.
+    """
     def requires(self):
-        gbasf2_input_dataset = os.path.join("/belle/MC/release-04-00-03/DB00000757/MC13a/prod00009434/s00/e1003/4S/",
-
-                                            "r00000/mixed/mdst/sub00/mdst_000255_prod00009434_task10020000255.root")
-        # create two analysis task for two different MBC cuts. Each will be its own gbasf2 project
-        for mbc_lower_cut in [5.15, 5.2]:
-            yield MyAnalysisTask(
-                mbc_range=(mbc_lower_cut, 5.3),
-                gbasf2_project_name_prefix="luigiExample",
-                gbasf2_input_dataset=gbasf2_input_dataset,
-                max_event=100,
-            )
+        gbasf2_input_datasets = [
+            "/belle/MC/release-04-00-03/DB00000757/MC13a/prod00009434/s00/e1003/4S/r00000/mixed/mdst/sub00",
+            "/belle/MC/release-04-00-03/DB00000757/MC13a/prod00009435/s00/e1003/4S/r00000/charged/mdst/sub00",
+        ]
+        # if you want to iterate over different cuts, just add more values to this list
+        mbc_lower_cuts = [5.15]
+        for mbc_lower_cut in mbc_lower_cuts:
+            for input_ds in gbasf2_input_datasets:
+                yield MyAnalysisTask(
+                    mbc_range=(mbc_lower_cut, 5.3),
+                    gbasf2_project_name_prefix="luigiExampleMultiDS",
+                    gbasf2_input_dataset=input_ds,
+                    max_event=100,
+                )
 
 
 if __name__ == '__main__':
