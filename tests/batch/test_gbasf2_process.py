3c8f0eff179aa6a360fddfeb61fdf2378cbcb849
Artur Gottmann <artur.gottmann@desy.de>
2021-04-27 14:05:16 +0200

    test_gbasf2_process.py: adapt again the dict

--- tests/batch/test_gbasf2_process.py
+++ tests/batch/test_gbasf2_process.py
@@ -73,7 +73,7 @@ class TestGbasf2RescheduleJobs(B2LuigiTestCase):
         self.gb2_mock_process.task = MyGbasf2Task("some_parameter")
         self.gb2_mock_process.dirac_user = "username"
         self.gb2_mock_process.gbasf2_project_name = get_unique_project_name(self.gb2_mock_process.task)
-        self.gb2_mock_process.n_retries_by_job = self.gb2_mock_process.task.n_retries_by_job
+        self.gb2_mock_process.n_retries_by_job = Gbasf2Process.n_retries_by_job
         self.gb2_mock_process.max_retries = 0
         b2luigi.set_setting("gbasf2_print_status_updates", False)
 
