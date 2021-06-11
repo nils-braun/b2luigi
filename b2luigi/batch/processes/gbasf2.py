661f27d08780e523ac2dcbc1305f3deda468ac81
Michael Eliachevitch <m.eliachevitch@posteo.de>
2021-06-11 11:23:11 +0200

    Add status messages for gbasf2 tasks

--- b2luigi/batch/processes/gbasf2.py
+++ b2luigi/batch/processes/gbasf2.py
@@ -46,6 +46,11 @@ class Gbasf2Process(BatchProcess):
           task with the same project name is running, this b2luigi gbasf2 wrapper will recognize that
           and instead of resubmitting a new project, continue monitoring the running project.
 
+          Tipp: The outputs of gbasf2 tasks can be a bit overwhelming, so I recommend using the
+            :ref`Start a Central Scheduler <central scheduler>`
+            which features a nice overview in the browser, including progress bars showing how many jobs in each
+            project are already done.
+
         - **Automatic download of datasets and logs**
 
           If all jobs had been successful, it automatically downloads the output dataset and
@@ -295,11 +300,12 @@ class Gbasf2Process(BatchProcess):
         # task is running
         if n_in_final_state < n_jobs:
             # try updating progressbar for central scheduler web view
-            try:
-                percentage = n_done / n_jobs * 100
-                self._scheduler.set_task_progress_percentage(self.task.task_id, percentage)
-            except AttributeError:
-                pass
+            percentage = n_done / n_jobs * 100
+            self._scheduler.set_task_progress_percentage(self.task.task_id, percentage)
+
+            status_message = "\n".join(f"{status}: {n_jobs}" for status, n_jobs in n_jobs_by_status.items())
+            self._scheduler.set_task_status_message(self.task.task_id, status_message)
+
             return JobStatus.running
 
         # Require all jobs to be done for project success, any job failure results in a failed project
