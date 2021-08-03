752920bcadf804127a48287b20627d09d8f7e9d0
Michael Eliachevitch <m.eliachevitch@posteo.de>
2021-08-03 12:20:25 +0200

    Name main wrapper task AggregatorTask
    As suggested by Philip Grace and is used in systematic corrections framework

--- examples/basf2/basf2_chain_example.py
+++ examples/basf2/basf2_chain_example.py
@@ -97,7 +97,7 @@ class AnalysisTask(Basf2PathTask):
         yield self.add_to_output("B_n_tuple.root")
 
 
-class MainTask(Basf2nTupleMergeTask):
+class AggregatorTask(Basf2nTupleMergeTask):
     n_events = luigi.IntParameter()
 
     def requires(self):
@@ -106,4 +106,4 @@ class MainTask(Basf2nTupleMergeTask):
 
 
 if __name__ == "__main__":
-    luigi.process(MainTask(n_events=1), workers=4)
+    luigi.process(AggregatorTask(n_events=1), workers=4)
