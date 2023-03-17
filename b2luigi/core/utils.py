5e103f38d1f8970461eb9138fc9f3bfd691d0ae5
Michael Eliachevitch <m.eliachevitch@posteo.de>
2023-03-17 16:07:43 +0100

    Fix mutable default argument and some minor PEP8 style issues

--- b2luigi/core/utils.py
+++ b2luigi/core/utils.py
@@ -139,14 +139,15 @@ def flatten_to_list_of_dicts(inputs):
     return dict(joined_dict)
 
 
-def task_iterator(task, only_non_complete=False, already_seen_tasks = set()):
+def task_iterator(task, only_non_complete=False, already_seen_tasks=None):
+    if already_seen_tasks is None:
+        already_seen_tasks = set()
     if not only_non_complete or not task.complete():
         yield task
         for dep in task.deps():
             if dep.task_id in already_seen_tasks:
                 continue
-            already_seen_tasks.add(dep.task_id) 
-        
+            already_seen_tasks.add(dep.task_id)
             yield from task_iterator(dep, only_non_complete=only_non_complete, already_seen_tasks=already_seen_tasks)
 
 
