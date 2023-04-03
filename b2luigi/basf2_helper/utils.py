62dd7af94d42fa9dfbf75a6cc02855fe70c93f8d
Michael Eliachevitch <m.eliachevitch@posteo.de>
2023-04-03 12:23:50 +0200

    Revert to behaviour of returning "not_set" if basf2 is not setup
    Just raising errors breaks unit tests and is just a backwards incompatible
    behaviour. Changing it should be a separate PR, then unittests could be fixed by mocking.
    
    Also added more documentation.

--- b2luigi/basf2_helper/utils.py
+++ b2luigi/basf2_helper/utils.py
@@ -2,17 +2,24 @@ import os
 
 
 def get_basf2_git_hash():
+    """Return name of basf2 release or if local basf2 is used its version hash.
+
+    The version is equivalent to the version returned by ``basf2 --version``.
+
+    Returns ``\"not set\"``, if no basf2 release is set and basf2 cannot be imported.
+    """
     basf2_release = os.getenv("BELLE2_RELEASE")
 
-    if basf2_release == "head" or basf2_release is None:
-
-        try:
-            from basf2.version import get_version
-            basf2_hash = get_version
-        except ImportError:
-            from basf2.version import version
-            basf2_hash = version
-
-        basf2_release = basf2_hash()
-
-    return basf2_release
+    if basf2_release not in ("head", None):
+        return basf2_release
+    # else if BASF2_RELEASE environment variable is not set, get version hash of
+    # local basf2
+    try:
+        import basf2.version
+        # try both get_version method and version attribute, one of which
+        # should work depending on basf2 release
+        if hasattr(basf2.version, "get_version"):
+            return basf2.version.get_version()
+        return basf2.version.version
+    except ImportError:
+        return "not_set"
