3f74a458ff108b6d34e8b72c95d8672adb7631e1
Michael Eliachevitch <m.eliachevitch@posteo.de>
2023-12-18 16:24:58 +0100

    Bump version to 0.10.2-archived
    Further releases will be made under the official Belle II fork

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Nils Braun'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.10.2'
+release = "0.10.2-archived"
 
 
 # -- General configuration ---------------------------------------------------
